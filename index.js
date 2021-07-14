ec117de79e26ebf7e4901d7ef54c3bbd1af076e6
Matt Dunn-Rankin <matchu1993@gmail.com>
2021-07-13 19:31:15 -0700

    Don't import CSS by default
    I'm using this in a Next.js application, which disallows libraries from importing CSS themselves!
    
    https://nextjs.org/docs/messages/css-npm

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.jQuery = require('jquery');
 require('./mathquill.min.js');
-require('./mathquill.css');
+
+// NOTE: We don't import the CSS here. Instead, applications can do it separately:
+//       import "mathquill-webpack/mathquill.css";
 
 module.exports = window.MathQuill;
